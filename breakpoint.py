f90f9f21f0f39e1073459f29cfe9e77856382337
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2013-03-23 15:14:44 +0100

    version 1.0.0

--- breakpoint.py
+++ breakpoint.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 """
-Function Execution Progress Tracker
+Progress Tracker
 """
 
 # Python 2.7 Standard Library
@@ -15,32 +14,33 @@ import time
 __author__ = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
 __license__ = "MIT License"
 __url__ = "https://github.com/boisgera/breakpoint" 
-__version__ = None
+__version__ = "1.0.0"
 
-# Random Notes
+#
+# Mics. Notes
 # ------------------------------------------------------------------------------
 # keywords: breakpoint, timing, etc.
-
+#
 # Use partial result if any ? Demonstrate how a handler may decide to stop
 # the iteration if it takes too long ?
-
+#
 # Demonstrate how to migrate an example to breakpoint, without breakpoint
 # frequency control, then with it. Use a fibionnaci sequence example ?
-
+#
 # Show that the progress stuff is optional ? convention that None should be
 # return when you have no idea what the outcome is.
-
+#
 # Use iterators/generators instead of threads ? The values obtained are
 # progress < 1.0 until we obtain 1.0, then the last value is the result ?
 # Nah, go for a StopIteration and get the previous value ? But before,
 # we would try to use it as a progress ... end with yield 1.0, result ?
-
+#
 # Use the .send method to say if the frequency of the outputs should be
 # increased / decreased (that may be ignored). What kind of message ?
 # A frequency multiplier ? Yup. And the receiver is free to do something
 # for example only if multiplier is > 2 or < 1/2.
 #
-#rt2 = (1.0 - new_progress) / (new_progress - progress) * dt_
+# rt2 = (1.0 - new_progress) / (new_progress - progress) * dt_
 # way to smooth the stuff ? With a weight that
 # is more important for recent evaluations, but
 # tempered by a (possibly large) collection of
@@ -57,7 +57,7 @@ __version__ = None
 # -> introduce a 'beta' factor such as
 # beta = 0 -> instant. slope and beta = 1 -> total
 # slope ? Or fixed alpha ?
-
+#
 
 def breakpoint(dt=1.0, handler=None):
     """
@@ -107,8 +107,10 @@ def breakpoint(dt=1.0, handler=None):
         return broken_
     return broken
 
+#
 # Test
 # ------------------------------------------------------------------------------
+#
 
 def handler(p, e, r):
     print p, e, r
